c76751c58b60c816d1b4d2396128766e052819be
Alex-Shutov <girasimov1337@gmail.com>
2022-04-07 19:52:17 +0500

    Add Exceptions

---
+++ Template.Application/Common/Exceptions/NotFoundEntityException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Application.Common.Exceptions
+{
+    public class NotFoundEntityException : Exception
+    {
+        public NotFoundEntityException(string name, object key) : base(
+            $"Entity \"{name}\": key \"{key}\" was not found") {}
+    }
+}
---
+++ Template.Application/Common/Exceptions/NotInstanceOfBaseClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Application.Common.Exceptions
+{
+    public class NotInstanceOfBaseClass : Exception
+    {
+        public NotInstanceOfBaseClass(Type instanceType, Type baseType) : base($"Class {instanceType} is not based on a {baseType}") {}
+    }
+}
